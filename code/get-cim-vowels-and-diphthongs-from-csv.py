e4a7b3deb3fed05debbc94480c52890283a74e98
rolandocoto <rcoto@email.arizona.edu>
2019-07-24 19:10:49 -0600

    Added extraction of both vowels and diphthongs
    Added the capability to extract vowels that are not in diphthongs, and to distinguish those from the diphthongs.

---
+++ code/get-cim-vowels-and-diphthongs-from-csv.py
@@ -0,0 +1,51 @@
+import os
+import string
+import sys
+
+if __name__ == "__main__":
+
+	inFilePath = os.path.join("",sys.argv[1])
+	inputCSV = open(inFilePath, encoding='utf-8').readlines()
+
+	vowels = ["a", "e", "i", "o", "u", "ā", "ē", "ī", "ō", "ū"]
+	header = "phone,type,Filename,TextGridLabel,Word,PreviousLabel,FollowingLabel,start,end,duration,f0_0point,f0_10point,f0_20point,f0_25point,f0_30point,f0_33point,f0_40point,f0_50point,f0_60point,f0_67point,f0_70point,f0_75point,f0_80point,f0_90point,f0_100point,F1_midpoint,F2_midpoint,F3_midpoint,intensity_midpoint,next_Filename,next_TextGridLabel,next_Word,next_PreviousLabel,next_FollowingLabel,next_start,next_end,next_duration,next_f0_0point,next_f0_10point,next_f0_20point,next_f0_25point,next_f0_30point,next_f0_33point,next_f0_40point,next_f0_50point,next_f0_60point,next_f0_67point,next_f0_70point,next_f0_75point,next_f0_80point,next_f0_90point,next_f0_100point,next_F1_midpoint,next_F2_midpoint,next_F3_midpoint,next_intensity_midpoint"
+
+	lineNumber = 1
+	totLines = len(inputCSV)
+	res = header + "\r\n"
+
+	for line in inputCSV:
+
+		line = line.replace("\r", "")
+		line = line.replace("\n", "")
+
+		tabLine = line.split(",")
+		letter = tabLine[1]
+		word = tabLine[2]
+		prevLetter = tabLine[3]
+		nextLetter = tabLine[4]
+
+		# Get the next line
+		if (lineNumber < (totLines-1) and line != ""):
+			nextWord = inputCSV[lineNumber+1].split(",")[2] 
+
+		# If the long has a vowel
+		if (letter in vowels):
+
+			# And this vowel is part of a diphthong (i.e. if it's followed by another vowel within the same word)
+			if (nextLetter in vowels and
+			letter != nextLetter and
+			word == nextWord):
+			
+				res += letter + nextLetter + "," + "diphthong" + "," + line + inputCSV[lineNumber+1]
+				
+			# If it's a lone vowel (i.e. not followed by another vowel within the same word)
+			else:
+			
+				res += letter + "," + "vowel" + "," + line
+
+
+
+		lineNumber = lineNumber + 1
+
+	open("cim-phones.csv", "wb").write((res.encode('utf-8','replace')))
